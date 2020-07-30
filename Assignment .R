c19178c005250a3a89bb756c7e093f9205611cc9
lakshaykapr <68824350+lakshaykapr@users.noreply.github.com>
2020-07-31 03:18:22 +0530

    Add files via upload

--- Assignment .R
+++ Assignment .R
@@ -1,30 +1,35 @@
-##Question 1
+##Question 1.
 
-makeCacheMatrix <- function(x=matrix()) {
-    inv <- NULL
-    set <- function(y) {
+makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL
+    
+    setMatrix <- function(y) {
         x <<- y
-        inv <- NULL
+        invMatrix <<- NULL
     }
-    get <- function() x
-    setInverse <- function(inverse) inv<<- solve(x)
-    getInverse<- function() inv
-    list(set=set,
-    get=get,
-    setInverse=setInverse,
-    getInverse=getInverse)
+    
+    getMatrix <- function() x                              
+    setInverse <- function(inverse) invMatrix <<- inverse  
+    getInverse <- function() invMatrix                     
+    list(setMatrix = setMatrix, getMatrix = getMatrix,
+         setInverse = setInverse, getInverse = getInverse)
+    
 }
 
-##Question 2
+##Question 2.
 
 cacheSolve <- function(x, ...) {
-    inv <- x$getInverse()
-    if (!is.null(inv)) {
-        message("getting cached data")
-        return(inv)
+    
+    
+    invMatrix <- x$getInverse()
+    if(!is.null(invMatrix)) {                       
+        message("Getting Cached Invertible Matrix")    
+        return(invMatrix)                             
     }
-    mat <- x$get()
-    inv <- solve(mat, ...)
-    x$setInverse(inv)
-    inv
+    
+      
+    MatrixData <- x$getMatrix()                      
+    invMatrix <- solve(MatrixData, ...)             
+    x$setInverse(invMatrix)                          
+    return(invMatrix)                               
 }
